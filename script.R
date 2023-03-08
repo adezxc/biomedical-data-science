a044ee3b0a236d8319b1c8d0235ee12b76662c8f
KazimierasJasaitis <kazimieras.jasaitis@gmail.com>
2023-03-08 16:05:14 +0200

    comments

--- script.R
+++ script.R
@@ -1,14 +1,18 @@
 
 Sys.setlocale("LC_ALL", "C")
 
-dat <- readRDS("data.rds")
-tm  <- readRDS("tm.rds")
+dat <- readRDS("data.rds") #Patients
+tm  <- readRDS("tm.rds") #Symptom matrix
 
-allergy_indices <- grep("allerg", colnames(tm))
-test <- tm[,allergy_indices]
+allergy_indices <- grep("allerg", colnames(tm)) #Which columns are related to allergies
+test <- tm[,allergy_indices, drop = FALSE] #Load symptom matrix, where columns are related to allergies, into 'test'
 
-temp <- rowSums(test) > 0
-patients <- dat[temp,]
-allergies <- test[temp,]
+temp <- rowSums(test) > 0 #Does a patient have allergies as at least one of their symptoms
+patients <- dat[temp,] #Load patients, that have allergies as at least one of their symptoms, into 'patients'
+allergies <- test[temp, , drop = FALSE] #Load allergy matrix, where a patient has allergies as at least one of their symptoms, into 'allergies'
 rm(dat)
 rm(tm)
+allergies["528",]
+
+test[1,0]
+rownames(allergies)
